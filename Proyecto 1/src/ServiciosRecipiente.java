54f98ffe956c0b0922074dae0c1e4f32547f3d3b
RubenAlexCruz <rubenalexcruz@ciencias.unam.mx>
2025-03-07 18:29:39 -0600

    Update ServiciosRecipiente.java

--- Proyecto 1/src/ServiciosRecipiente.java
+++ Proyecto 1/src/ServiciosRecipiente.java
@@ -4,7 +4,7 @@
 public interface ServiciosRecipiente {
 
     /**
-    * Calcula y devuelve la cantidad m ́a xima de l ́ı quido que cabe en
+    * Calcula y devuelve la cantidad m axima de líquido que cabe en
     * el recipiente .
     *
     * @return La capacidad total .
@@ -22,33 +22,33 @@ public interface ServiciosRecipiente {
 
 
     /**
-    * Indica si el recipiente est  ́a vac  ́ıo, es decir , la cantidad
-    * de l ́ı quido que contiene es cero .
+    * Indica si el recipiente está vacío, es decir , la cantidad
+    * de líquido que contiene es cero .
     *
-    * @return true si est  ́a vac  ́ıo, false en otro caso .
+    * @return true si está vacío, false en otro caso .
     */
     public boolean estaVacio ();
 
 
     /**
-    * Indica si el recipiente est  ́a lleno , es decir , la cantidad
-    * de l ́ı quido que contiene es igual a la de su capacidad .
+    * Indica si el recipiente está lleno , es decir , la cantidad
+    * de líquido que contiene es igual a la de su capacidad .
     *
-    * @return true si est  ́a lleno , false en otro caso .
+    * @return true si está lleno , false en otro caso .
     */
     public boolean estaLleno ();
 
 
     /**
-    * Agrega una nueva cantidad de l ́ı quido al recipiente , respetando
-    * la capacidad m ́a xima del mismo . En caso de que no quepa todo
+    * Agrega una nueva cantidad de líquido al recipiente , respetando
+    * la capacidad máxima del mismo . En caso de que no quepa todo
     * el l ́ıquido , deber  ́a devolver la cantidad excedente .
     * Por ejemplo , si un recipiente tiene cantidad actual 15 y
     * capacidad total 20 , al rellenarlo con 10 deber  ́ıa actualizar
     * la cantidad actual a 20 y devolver un excedente de 5.
     *
-    * @param cantidad La cantidad de l ́ı quido a agregar .
-    * @return La cantidad excedente . Puede ser cero si cab  ́ıa todo .
+    * @param cantidad La cantidad de líquido a agregar .
+    * @return La cantidad excedente . Puede ser cero si cabía todo .
     */
     public double rellena ( double cantidad );
 
@@ -56,14 +56,14 @@ public interface ServiciosRecipiente {
     /**
     * Resetea la cantidad actual a cero .
     *
-    * @return La cantidad de l ́ı quido que pose  ́ıa.
+    * @return La cantidad de líquido que poseía.
     */
     public double vacia ();
 
 
     /**
     * Vierte el contenido del recipiente en otro respetando su
-    * capacidad m ́a xima . En caso de haber excedente , se debe guardar
+    * capacidad máxima . En caso de haber excedente , se debe guardar
     * en el recipiente invocante .
     * Por ejemplo , si un recipiente A tiene cantidad actual 10 y
     * vertemos su contenido en un recipiente B con cantidad actual 3
@@ -71,7 +71,7 @@ public interface ServiciosRecipiente {
     * recipiente A quede con cantidad actual 8 y el recipiente B con
     * cantidad actual 5.
     *
-    * @param otro El recipiente donde se vertir  ́a el l ́ı quido actual .
+    * @param otro El recipiente donde se vertir á el líquido actual .
     */
     public void vierte ( Recipiente otro );
 
@@ -79,9 +79,9 @@ public interface ServiciosRecipiente {
     /**
     * Indica si el recipiente tiene exactamente las mismas
     * dimensiones que otro . Solo revisamos las dimensiones , la
-    * cantidad de l ́ı quido puede variar .
+    * cantidad de líquido puede variar .
     *
-    * @param otro El recipiente con que se comparar  ́a.
+    * @param otro El recipiente con que se comparará.
     * @return true si tienen las mismas dimensiones , false en otro
     * caso .
     */
@@ -93,7 +93,7 @@ public interface ServiciosRecipiente {
     * capacidad total que otro . Solo revisamos la capacidad total ,
     * las dimensiones pueden variar .
     *
-    * @param otro El recipiente con que se comparar  ́a.
+    * @param otro El recipiente con que se comparará.
     * @return true si tienen la misma capacidad total , false en otro
     * caso .
     */
@@ -103,7 +103,7 @@ public interface ServiciosRecipiente {
     /**
     * Indica si el recipiente tiene mayor cantidad actual que otro .
     *
-    * @param otro El recipiente con que se comparar  ́a.
+    * @param otro El recipiente con que se comparará.
     * @return true si tiene mayor cantidad , false en otro caso .
     */
     public boolean contieneMas ( Recipiente otro );
@@ -113,7 +113,7 @@ public interface ServiciosRecipiente {
     * Indica si el recipiente tiene mayor capacidad restante que
     * otro .
     *
-    * @param otro El recipiente con que se comparar  ́a.
+    * @param otro El recipiente con que se comparará.
     * @return true si tiene mayor capacidad restante , false en otro
     * caso .
     */
@@ -121,17 +121,13 @@ public interface ServiciosRecipiente {
 
 
     /**
-    * Crea un nuevo recipiente vac  ́ıo cuya capacidad total es igual
+    * Crea un nuevo recipiente vacío cuya capacidad total es igual
     * a la cantidad actual del recipiente invocante .
     * Por ejemplo , a partir de un recipiente A de altura h y radio
-    * de su base r que est  ́a lleno solo hasta la mitad , se podr  ́ıa
-    * generar un recipiente vac  ́ıo B de altura
-    h
-    2
-    y radio de su base r.
-
+    * de su base r que está lleno solo hasta la mitad , se podría
+    * generar un recipiente vacío B de altura h/2 y radio de su base r.
     *
-    * @return Un nuevo recipiente vac  ́ıo con capacidad total igual a
+    * @return Un nuevo recipiente vacío con capacidad total igual a
     * la cantidad actual del recipiente invocante .
     */
     public Recipiente creaContenedorJusto ();
