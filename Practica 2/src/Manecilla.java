03a66556ffcd252c123c05f3292c3a19d1258f96
RubenAlexCruz <rubenalexcruz@ciencias.unam.mx>
2025-03-15 16:56:12 -0600

    Update Manecilla.java

--- Practica 2/src/Manecilla.java
+++ Practica 2/src/Manecilla.java
@@ -17,11 +17,20 @@ public class Manecilla {
         // Se utiliza un if para verificar que el valor no exceda el límite y si sí simplemente resetea el mod del valor respecto al límite
     }
 
+    /**
+     * Get para el valor de una manecilla
+     * @return el valor de la manecilla
+     */
     public int getValor(){
         return this.valor;
     }
 
-    //igual que al construir una manecilla se utiliza  un if para verificar que el valor no exceda el límite
+    /**
+     * Set para el valor de una manecilla
+     * Se utiliza un if para verificar que el valor no exceda el límite
+     * 
+     * @param valor el valor que al que se quiere set
+     */
     public void setValor(int valor){
         if (valor >= this.limite){
             this.valor = valor % this.limite;
