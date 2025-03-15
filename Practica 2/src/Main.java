7bedb050b3d4d01f7aafde513a54dbd46eaf884c
RubenAlexCruz <rubenalexcruz@ciencias.unam.mx>
2025-03-15 16:54:30 -0600

    Update Main.java

--- Practica 2/src/Main.java
+++ Practica 2/src/Main.java
@@ -1,8 +1,13 @@
+import java.util.Scanner; 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        Reloj reloj = new Reloj();
-        //Pruebas de la funcionalidad de los metodos de la clase reloj borre el comentario para accionarlos
 
+ 
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        Reloj reloj = new Reloj();
+
+        //Pruebas de la funcionalidad de los metodos de la clase reloj. borre el comentario para accionarlos
+        /* 
         System.out.println("Prueba del metodo mostrarAnalogo en ceros");
         System.out.println(reloj.mostrarAnalogo());
         System.out.println();
@@ -24,6 +29,62 @@ public class Main {
         System.out.println("Hora actual: " + reloj.getHora());
         System.out.println("Minuto actual: " + reloj.getMinutos());
         System.out.println("Segundo actual: " + reloj.getSegundos());
+        System.out.println("\nPrueba de que los metodos agregarSegundos y agregarMinutos suman minutos y horas apropiadamente");
+        reloj.agregarSegundos(300); 
+        System.out.println("\nSe agregan 300 segundos lo cual agrega 5 minutos al reloj:");
+        System.out.println(reloj.mostrarAnalogo()+"\n");
+        System.out.println("\nSe agregan 280 minutos lo cual agrega 4 horas y 40 minutos al reloj: ")
+        reloj.agregarMinutos(280); 
+        System.out.println(reloj.mostrarAnalogo());
+        */
 
+        System.out.println("Reloj");
+        System.out.println("Ingrese 1 para accionar el reloj con un loop for \nIngrese 2 para accionar el reloj con un metodo while \nIngrese 3 para accionar el reloj con un metodo do while ");
+        int resp = scanner.nextInt();
+        
+        switch (resp) {
+            case 1:
+                reloj.setHoras(11);
+                reloj.setMinutos(55);
+                reloj.setSegundos(30);
+                for (int i = 0 ; i < 12 ; i++ ) {
+                    System.out.println(reloj.mostrarAnalogo());
+                    reloj.agregarSegundos(30);
+                    Thread.sleep(1000);
+                }
+                break;
+            case 2:
+                int i = 0;
+                reloj.setHoras(11);
+                reloj.setMinutos(52);
+                reloj.setSegundos(0);
+                while (i <= 12 ) {
+                    System.out.println(reloj.mostrarAnalogo());
+                    reloj.agregarSegundos(5);
+                    reloj.agregarMinutos(1);
+                    i++;
+                    Thread.sleep(1000);
+                }
+                break;
+            case 3: 
+                reloj.setHoras(7);
+                reloj.setMinutos(2);
+                reloj.setSegundos(20);
+                int j = 0;
+                do { 
+                    System.out.println(reloj.mostrarAnalogo());
+                    reloj.agregarHoras(1);
+                    reloj.agregarMinutos(8);
+                    reloj.agregarSegundos(20);
+                    j++;
+                    Thread.sleep(1000);
+                } while (j < 8);
+                
+                break;
+            default:
+                System.out.println("Error porfavor ingrese únicamente 1, 2 o 3");
+        }
+
+ 
     }
 }
